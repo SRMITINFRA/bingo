f60d3454e4c80b7e4fda87122d6e0c5457f15799
SRMITINFRA <srmitinfra@outlook.com>
2026-06-28 16:19:34 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,23 @@
-const CACHE_NAME = 'bingo-royale-v1';
+const CACHE_NAME = 'bingo-royale-v2';
 const ASSETS = [
+  './',
   './index.html',
   './live.html',
-  './manifest.json'
+  './manifest.json',
+  './icon192.png',
+  './icon512.png'
 ];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
-    })
+    }).then(() => self.skipWaiting())
   );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', (e) => {
