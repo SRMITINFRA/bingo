1febcca4873a6af0000fb3b92df6e81bc35d93eb
SRMITINFRA <srmitinfra@outlook.com>
2026-06-28 16:10:10 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'bingo-royale-v1';
+const ASSETS = [
+  './index.html',
+  './live.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
